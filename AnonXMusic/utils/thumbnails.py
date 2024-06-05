e8276601c902a6623918c1c3a9e9dc5667ef0a51
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-05 14:59:44 +0530

    Update thumbnails.py
    Signed-off-by: GDN Bharath  <148096435+dattudd@users.noreply.github.com>

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -6,7 +6,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
-from InflexMusic import app
+from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
 
 def changeImageSize(maxWidth, maxHeight, image):
